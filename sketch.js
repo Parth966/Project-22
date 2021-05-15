f450986a54ece8223134d4188db74d7d6ac0fc3e
Parth966 <70880284+Parth966@users.noreply.github.com>
2021-05-15 15:05:43 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+var starImg,bgImg;
+var star, starBody;
+//create variable for fairy sprite and fairyImg
+var f,f1;
+var fv;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{   fv = loadSound("sound/JoyMusic.mp3")
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	//load animation for fairy here
+	f1 = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	//write code to play fairyVoice sound
+	fv.play();
+	
+
+	//create fairy sprite and add animation for fairy
+    f = createSprite(200,450,50,50);
+	f.addAnimation("fairy",f1);
+	f.scale = 0.2;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+  keyPressed();
+  star.x= starBody.position.x ;
+  star.y= starBody.position.y ;
+
+  console.log(star.y);
+
+  //write code to stop star in the hand of fairy
+  if ( star.y > 430 && starBody.position.y > 430 && star.isTouching(f)){
+	  Matter.Body.setStatic(starBody,true);
+  }
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//writw code to move fairy left and right
+	if (keyWentDown("LEFT_ARROW")){
+       f.x = f.x - 50
+	}
+	if (keyWentDown("RIGHT_ARROW")){
+		f.x = f.x + 50
+	}
+        
+}
